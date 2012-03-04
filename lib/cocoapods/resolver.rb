rulien/CocoaPods

No longer cache Specification::Set in the Set class, but in a Context.

lib/cocoapods/resolver.rb
@@ -1,20 +1,56 @@
 module Pod
   class Resolver
-    def initialize(specification, dependencies = nil)
-      @specification, @dependencies = specification, dependencies || specification.dependencies
+    # A Resolver::Context caches specification sets and is used by the resolver
+    # to ensure that extra dependencies on a set are added to the same instance.
+    #
+    # In addition, the context is later on used by Specification to lookup other
+    # specs, like the on they are a part of.
+    class Context
+      attr_reader :sources, :sets
+
+      def initialize
+        @sets = {}
+        @sources = Source::Aggregate.new
+      end
+
+      def find_dependency_set(dependency)
+        @sets[dependency.name] ||= begin
+          if external_spec = dependency.specification
+            Specification::Set::External.new(external_spec)
+          else
+            @sources.search(dependency)
+          end
+        end
+      end
     end
 
-    def resolve
-      @sets, @loaded_spec_names, @specs = [], [], []
-      find_dependency_sets(@specification, @dependencies)
-      @specs.sort_by(&:name)
+    attr_reader :context
+
+    def initialize
+      @context = Context.new
     end
+
+    def resolve(specification, top_level_dependencies = nil)
+      @specs = {}
+      @top_level_specification = specification
+      find_dependency_sets(specification, top_level_dependencies)
+      @specs.values.sort_by(&:name).each do |spec|
+        if spec.part_of_other_pod?
+          # Specification doesn't need to know more about a context, so we assign
+          # the other specification, of which this pod is a part, to the spec.
+          spec.part_of_specification = @context.sets[spec.part_of.name].specification
+        end
+      end
+    end
+
+    private
 
     def find_dependency_sets(specification, dependencies = nil)
       (dependencies || specification.dependencies).each do |dependency|
-        set = find_dependency_set(dependency)
+        set = @context.find_dependency_set(dependency)
         set.required_by(specification)
-        unless @loaded_spec_names.include?(dependency.name)
+        # Ensure we don't resolve the same spec twice
+        unless @specs.has_key?(dependency.name)
           # Get a reference to the spec that’s actually being loaded.
           # If it’s a subspec dependency, e.g. 'RestKit/Network', then
           # find that subspec.
@@ -23,28 +59,16 @@ module Pod
             spec = spec.subspec_by_name(dependency.name)
           end
           validate_platform!(spec)
-
-          # Ensure we don't resolve the same spec twice
-          @loaded_spec_names << spec.name
-          @specs << spec
-          @sets << set unless @sets.include?(set)
-
+          @specs[spec.name] = spec
+          # And recursively load the dependencies of the spec.
           find_dependency_sets(spec)
         end
       end
     end
 
-    def find_dependency_set(dependency)
-      if external_spec = dependency.specification
-        Specification::Set::External.new(external_spec)
-      else
-        Source.search(dependency)
-      end
-    end
-
     def validate_platform!(spec)
-      unless spec.platform.nil? || spec.platform == @specification.platform
-        raise Informative, "The platform required by the Podfile (:#{@specification.platform}) " \
+      unless spec.platform.nil? || spec.platform == @top_level_specification.platform
+        raise Informative, "The platform required by the Podfile (:#{@top_level_specification.platform}) " \
                            "does not match that of #{spec} (:#{spec.platform})"
       end
     end

CocoaPods/Core

lib/cocoapods/resolver.rb
@@ -1,26 +1,72 @@
 module Pod
   class Resolver
-    def initialize(podfile, sandbox)
-      @podfile = podfile
-      @sandbox = sandbox
-    end
+    # A Resolver::Context caches specification sets and is used by the resolver
+    # to ensure that extra dependencies on a set are added to the same instance.
+    #
+    # In addition, the context is later on used by Specification to lookup other
+    # specs, like the on they are a part of.
+    class Context
+      attr_reader :sources, :sets, :sandbox
 
-    def resolve
-      @podfile.target_definitions.values.inject({}) do |result, target_definition|
-        @sets, @loaded_spec_names, @specs = [], [], []
-        find_dependency_sets(@podfile, target_definition.dependencies)
-        result[target_definition] = @specs.sort_by(&:name)
-        result
+      def initialize(sandbox)
+        @sandbox = sandbox
+        @sets    = {}
+        @sources = Source::Aggregate.new
+      end
+
+      def find_dependency_set(dependency)
+        @sets[dependency.name] ||= begin
+          if dependency.specification
+            Specification::Set::External.new(dependency.specification)
+          elsif external_source = dependency.external_source
+            specification = external_source.specification_from_sandbox(@sandbox)
+            Specification::Set::External.new(specification)
+          else
+            @sources.search(dependency)
+          end
+        end
       end
     end
 
+    attr_reader :podfile, :sandbox
+    attr_accessor :context
+
+    def initialize(podfile, sandbox)
+      @podfile = podfile
+      @sandbox = sandbox
+      @context = Context.new(@sandbox)
+    end
+
+    def resolve
+      @specs = {}
+
+      result = @podfile.target_definitions.values.inject({}) do |result, target_definition|
+        @loaded_specs = []
+        find_dependency_sets(@podfile, target_definition.dependencies)
+        result[target_definition] = @specs.values_at(*@loaded_specs).sort_by(&:name)
+        result
+      end
+
+      # Specification doesn't need to know more about a context, so we assign
+      # the other specification, of which this pod is a part, to the spec.
+      @specs.values.sort_by(&:name).each do |spec|
+        if spec.part_of_other_pod?
+          spec.part_of_specification = @context.sets[spec.part_of.name].specification
+        end
+      end
+
+      result
+    end
+
+    private
+
     # this can be called with anything that has dependencies
     # e.g. a Specification or a Podfile.
-    def find_dependency_sets(has_dependencies, dependency_subset = nil)
-      (dependency_subset || has_dependencies.dependencies).each do |dependency|
-        set = find_dependency_set(dependency)
-        set.required_by(has_dependencies)
-        unless @loaded_spec_names.include?(dependency.name)
+    def find_dependency_sets(specification, dependencies = nil)
+      (dependencies || specification.dependencies).each do |dependency|
+        set = @context.find_dependency_set(dependency)
+        set.required_by(specification)
+        unless @loaded_specs.include?(dependency.name)
           # Get a reference to the spec that’s actually being loaded.
           # If it’s a subspec dependency, e.g. 'RestKit/Network', then
           # find that subspec.
@@ -28,26 +74,15 @@ module Pod
           if dependency.subspec_dependency?
             spec = spec.subspec_by_name(dependency.name)
           end
+
           validate_platform!(spec)
 
-          # Ensure we don't resolve the same spec twice
-          @loaded_spec_names << spec.name
-          @specs << spec
-          @sets << set unless @sets.include?(set)
+          @loaded_specs << spec.name
+          @specs[spec.name] = spec
 
+          # And recursively load the dependencies of the spec.
           find_dependency_sets(spec)
         end
-      end
-    end
-
-    def find_dependency_set(dependency)
-      if dependency.specification
-        Specification::Set::External.new(dependency.specification)
-      elsif external_source = dependency.external_source
-        specification = external_source.specification_from_sandbox(@sandbox)
-        Specification::Set::External.new(specification)
-      else
-        Source.search(dependency)
       end
     end
 