414d1fa05d6e41f22614bb4aed33e983f9615a9a
Fabio Pelosin <fabiopelosin@gmail.com>
2012-10-28 16:41:26 +0100

    [Core Extraction] Statistics cleanup and specs.

--- spec/specification/statistics_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../../spec_helper', __FILE__)
-
-describe Pod::Specification::Statistics do
-  before do
-    fixture('banana-lib') # ensure the archive is unpacked
-    @spec = Pod::Specification.from_file(fixture('banana-lib/BananaLib.podspec'))
-  end
-end
-
