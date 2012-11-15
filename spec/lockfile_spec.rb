570308773c22dee1ad9398b854f924794c1b1803
Fabio Pelosin <fabiopelosin@gmail.com>
2012-11-15 19:25:00 +0100

    [Lockfile] Clean up.

--- spec/lockfile_spec.rb
+++ spec/lockfile_spec.rb
@@ -1,38 +1,51 @@
 require File.expand_path('../spec_helper', __FILE__)
 
-describe Pod::Lockfile do
+module Pod
 
-  describe "In general" do
-    extend SpecHelper::TemporaryDirectory
+  class Sample
+    extend SpecHelper::Fixture
 
-    def sample
+    def self.yaml
       text = <<-LOCKFILE.strip_heredoc
-        PODS:
-        - BananaLib (1.0):
-          - monkey (< 1.0.9, ~> 1.0.1)
-        - monkey (1.0.8)
+      PODS:
+      - BananaLib (1.0):
+        - monkey (< 1.0.9, ~> 1.0.1)
+      - JSONKit (1.4)
+      - monkey (1.0.8)
 
-        DEPENDENCIES:
-        - BananaLib (~> 1.0)
+      DEPENDENCIES:
+      - BananaLib (~> 1.0)
+      - JSONKit (from `path/JSONKit.podspec')
 
-        SPEC CHECKSUMS:
-          BananaLib: 9906b267592664126923875ce2c8d03824372c79
+      EXTERNAL SOURCES:
+        JSONKit:
+          :podspec: path/JSONKit.podspec
 
-        COCOAPODS: #{Pod::CORE_VERSION}
+      SPEC CHECKSUMS:
+        BananaLib: 9906b267592664126923875ce2c8d03824372c79
+        JSONKit: 92ae5f71b77c8dec0cd8d0744adab79d38560949
+
+      COCOAPODS CORE: #{CORE_VERSION}
       LOCKFILE
     end
 
-    def podfile
-      Pod::Podfile.new do
+    def self.podfile
+      Podfile.new do
         platform :ios
         pod 'BananaLib', '~>1.0'
+        pod 'JSONKit', :podspec => 'path/JSONKit.podspec'
       end
     end
 
-    def specs
+    def self.specs
+      repo_path      = 'spec-repos/test_repo/'
+      bananalib_path = repo_path + 'BananaLib/1.0/BananaLib.podspec'
+      jsonkit_path   = repo_path + 'JSONKit/1.4/JSONKit.podspec'
+
       specs = [
-        Pod::Specification.from_file(fixture('BananaLib.podspec')),
-        Pod::Specification.new do |s|
+        Specification.from_file(fixture(bananalib_path)),
+        Specification.from_file(fixture(jsonkit_path)),
+        Specification.new do |s|
           s.name = "monkey"
           s.version = "1.0.8"
         end
@@ -40,259 +53,208 @@ describe Pod::Lockfile do
       specs.each { |s| s.activate_platform(:ios) }
       specs
     end
-
-    def tmp_path
-      temporary_directory + 'Podfile.lock'
-    end
-
-    it "loads from a hash" do
-      lockfile = Pod::Lockfile.new(YAML.load(sample))
-      lockfile.to_hash.should == YAML.load(sample)
-    end
-
-    it "loads from a file" do
-      File.open(tmp_path, 'w') {|f| f.write(sample) }
-      lockfile = Pod::Lockfile.from_file(tmp_path)
-      lockfile.defined_in_file.should == tmp_path
-      lockfile.to_hash.should == YAML.load(sample)
-    end
-
-    it "can be generated from a Podfile and a list of Specifications" do
-      lockfile = Pod::Lockfile.generate(podfile, specs)
-      lockfile.to_hash.should == YAML.load(sample)
-    end
-
-    before do
-      @lockfile = Pod::Lockfile.generate(podfile, specs)
-    end
-
-    it "generates a valid YAML representation" do
-      YAML.load(@lockfile.to_yaml).should == YAML.load(sample)
-    end
-
-    it "generates a valid Dictionary representation" do
-      @lockfile.to_hash.should == YAML.load(sample)
-    end
-
-    it "returns the list of the installed pods" do
-      @lockfile.pods_names.should == %w| BananaLib monkey |
-    end
-
-    it "returns the versions of the installed pods" do
-      @lockfile.pods_versions.should == {
-        "BananaLib" => Pod::Version.new("1.0"),
-        "monkey" => Pod::Version.new("1.0.8")
-      }
-    end
-
-    it "serializes correctly `:head' dependencies" do
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BananaLib', :head
-      end
-      specs = [
-        Pod::Specification.new do |s|
-          s.name = "BananaLib"
-          s.version = "1.0"
-        end,
-        Pod::Specification.new do |s|
-          s.name = "monkey"
-          s.version = "1.0.8"
-        end
-      ]
-      specs.each { |s| s.activate_platform(:ios) }
-      lockfile = Pod::Lockfile.generate(podfile, specs)
-      lockfile.to_hash["DEPENDENCIES"][0].should == "BananaLib (HEAD)"
-    end
-
-    it "serializes correctly external dependencies" do
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BananaLib', { :git => "www.example.com", :tag => '1.0' }
-      end
-      specs = [
-        Pod::Specification.new do |s|
-          s.name = "BananaLib"
-          s.version = "1.0"
-        end,
-        Pod::Specification.new do |s|
-          s.name = "monkey"
-          s.version = "1.0.8"
-        end
-      ]
-      specs.each { |s| s.activate_platform(:ios) }
-      lockfile = Pod::Lockfile.generate(podfile, specs)
-      lockfile.to_hash["DEPENDENCIES"][0].should == "BananaLib (from `www.example.com', tag `1.0')"
-      lockfile.to_hash["EXTERNAL SOURCES"]["BananaLib"].should == { :git => "www.example.com", :tag => '1.0' }
-    end
-
-    it "creates a dependency from a string" do
-      d = @lockfile.dependency_from_string("BananaLib (1.0)")
-      d.name.should == "BananaLib"
-      d.requirement.should =~ Pod::Version.new("1.0")
-      d.head.should.be.nil
-      d.external_source.should.be.nil
-    end
-
-    it "creates a head dependency from a string" do
-      d = @lockfile.dependency_from_string("BananaLib (HEAD)")
-      d.name.should == "BananaLib"
-      d.requirement.should.be.none?
-      d.head.should.be.true
-      d.external_source.should.be.nil
-    end
-
-    it "creates an external dependency from a string" do
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BananaLib', { :git => "www.example.com", :tag => '1.0' }
-      end
-      lockfile = Pod::Lockfile.generate(podfile, [])
-      d = lockfile.dependency_from_string("BananaLib (from `www.example.com', tag `1.0')")
-      d.name.should == "BananaLib"
-      d.requirement.should.be.none?
-      d.external?.should.be.true
-      d.external_source[:git].should == "www.example.com"
-      d.external_source[:tag].should == '1.0'
-    end
   end
 
-  describe "Initialization from a file" do
-    extend SpecHelper::TemporaryDirectory
+  #---------------------------------------------------------------------------#
 
-    it "returns nil if it can't find the initialization file" do
-      lockfile = Pod::Lockfile.from_file(temporary_directory + 'Podfile.lock_not_existing')
-      lockfile.should == nil
-    end
-  end
+  describe Lockfile do
+    describe "In general" do
+      extend SpecHelper::TemporaryDirectory
 
-  describe "Comparison with a Podfile" do
-    before do
-      @podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BlocksKit'
-        pod 'JSONKit'
+      before do
+        @tmp_path = temporary_directory + 'Podfile.lock'
       end
-      @specs = [
-        Pod::Specification.new do |s|
-          s.name = "BlocksKit"
-          s.version = "1.0.0"
-        end,
-        Pod::Specification.new do |s|
-          s.name = "JSONKit"
-          s.version = "1.4"
-        end ]
+
+      it "stores the initialization hash" do
+        lockfile = Lockfile.new(YAML.load(Sample.yaml))
+        lockfile.internal_data.should == YAML.load(Sample.yaml)
+      end
+
+      it "loads from a file" do
+        File.open(@tmp_path, 'w') {|f| f.write(Sample.yaml) }
+        lockfile = Lockfile.from_file(@tmp_path)
+        lockfile.internal_data.should == YAML.load(Sample.yaml)
+      end
+
+      it "returns nil if it can't find the initialization file" do
+        lockfile = Lockfile.from_file(temporary_directory + 'Podfile.lock_not_existing')
+        lockfile.should == nil
+      end
+
+      it "returns the file in which is defined" do
+        File.open(@tmp_path, 'w') {|f| f.write(Sample.yaml) }
+        lockfile = Lockfile.from_file(@tmp_path)
+        lockfile.defined_in_file.should == @tmp_path
+      end
+
+      #--------------------------------------#
+
+      before do
+        @lockfile = Lockfile.generate(Sample.podfile, Sample.specs)
+      end
+
+      it "returns the list of the names of the  installed pods" do
+        @lockfile.pod_names.should == %w| BananaLib JSONKit monkey |
+      end
+
+      it "returns the versions of the installed pods" do
+        @lockfile.pod_versions.should == {
+          "BananaLib" => Version.new("1.0"),
+          "JSONKit" => Version.new("1.4"),
+          "monkey" => Version.new("1.0.8")
+        }
+      end
+
+      it "returns the dependencies used for the last installation" do
+        json_dep = Dependency.new('JSONKit')
+        json_dep.external_source = { :podspec => 'path/JSONKit.podspec' }
+        @lockfile.dependencies.should == [
+          Dependency.new('BananaLib', '~>1.0'),
+          json_dep
+        ]
+      end
+
+      it "includes the external source information in the generated dependencies" do
+        dep = @lockfile.dependencies.find { |d| d.name == 'JSONKit' }
+        dep.external_source.should == { :podspec => 'path/JSONKit.podspec' }
+      end
+
+      it "returns the dependency that locks the pod with the given name to the installed version" do
+        json_dep = Dependency.new('JSONKit', '1.4')
+        json_dep.external_source = { :podspec => 'path/JSONKit.podspec' }
+        @lockfile.dependency_to_lock_pod_named('JSONKit').should == json_dep
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "Comparison with a Podfile" do
+      before do
+        @podfile = Podfile.new do
+          platform :ios
+          pod 'BlocksKit'
+          pod 'JSONKit'
+        end
+        @specs = [
+          Specification.new do |s|
+            s.name = "BlocksKit"
+            s.version = "1.0.0"
+          end,
+          Specification.new do |s|
+            s.name = "JSONKit"
+            s.version = "1.4"
+          end ]
         @specs.each { |s| s.activate_platform(:ios) }
-        @lockfile = Pod::Lockfile.generate(@podfile, @specs)
-    end
+        @lockfile = Lockfile.generate(@podfile, @specs)
+      end
 
-    it "detects an added Pod" do
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BlocksKit'
-        pod 'JSONKit'
-        pod 'TTTAttributedLabel'
+      it "detects an added Pod" do
+        podfile = Podfile.new do
+          platform :ios
+          pod 'BlocksKit'
+          pod 'JSONKit'
+          pod 'TTTAttributedLabel'
+        end
+        @lockfile.detect_changes_with_podfile(podfile).should == {
+          :changed=>[],
+          :removed=>[],
+          :unchanged=>["BlocksKit", "JSONKit"],
+          :added=>["TTTAttributedLabel"]
+        }
       end
-      @lockfile.detect_changes_with_podfile(podfile).should == {
-        :changed=>[],
-        :removed=>[],
-        :unchanged=>["BlocksKit", "JSONKit"],
-        :added=>["TTTAttributedLabel"]
-      }
-    end
 
-    it "detects an removed Pod" do
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BlocksKit'
+      it "detects an removed Pod" do
+        podfile = Podfile.new do
+          platform :ios
+          pod 'BlocksKit'
+        end
+        @lockfile.detect_changes_with_podfile(podfile).should == {
+          :changed=>[],
+          :removed=>["JSONKit"],
+          :unchanged=>["BlocksKit"],
+          :added=>[]
+        }
       end
-      @lockfile.detect_changes_with_podfile(podfile).should == {
-        :changed=>[],
-        :removed=>["JSONKit"],
-        :unchanged=>["BlocksKit"],
-        :added=>[]
-      }
-    end
 
-    it "detects Pods whose version changed" do
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BlocksKit'
-        pod 'JSONKit', "> 1.4"
+      it "detects Pods whose version changed" do
+        podfile = Podfile.new do
+          platform :ios
+          pod 'BlocksKit'
+          pod 'JSONKit', "> 1.4"
+        end
+        @lockfile.detect_changes_with_podfile(podfile).should == {
+          :changed=>["JSONKit"],
+          :removed=>[],
+          :unchanged=>["BlocksKit"],
+          :added=>[]
+        }
       end
-      @lockfile.detect_changes_with_podfile(podfile).should == {
-        :changed=>["JSONKit"],
-        :removed=>[],
-        :unchanged=>["BlocksKit"],
-        :added=>[]
-      }
-    end
 
-    it "it doesn't mark a changed Pods whose version changed but is still compatible with the Podfile" do
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BlocksKit'
-        pod 'JSONKit', "> 1.0"
+      it "it doesn't mark as changed Pods whose version changed but is still compatible with the Podfile" do
+        podfile = Podfile.new do
+          platform :ios
+          pod 'BlocksKit'
+          pod 'JSONKit', "> 1.0"
+        end
+        @lockfile.detect_changes_with_podfile(podfile).should == {
+          :changed=>[],
+          :removed=>[],
+          :unchanged=>["BlocksKit", "JSONKit"],
+          :added=>[]
+        }
       end
-      @lockfile.detect_changes_with_podfile(podfile).should == {
-        :changed=>[],
-        :removed=>[],
-        :unchanged=>["BlocksKit", "JSONKit"],
-        :added=>[]
-      }
-    end
 
-    it "detects Pods whose external source changed" do
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BlocksKit'
-        pod 'JSONKit', :git => "example1.com"
+      it "detects Pods whose external source changed" do
+        podfile = Podfile.new do
+          platform :ios
+          pod 'BlocksKit'
+          pod 'JSONKit', :git => "example1.com"
+        end
+        @lockfile.detect_changes_with_podfile(podfile).should == {
+          :changed=>["JSONKit"],
+          :removed=>[],
+          :unchanged=>["BlocksKit"],
+          :added=>[]
+        }
+        @lockfile = Lockfile.generate(podfile, @specs)
+        podfile = Podfile.new do
+          platform :ios
+          pod 'BlocksKit'
+          pod 'JSONKit', :git => "example2.com"
+        end
+        @lockfile.detect_changes_with_podfile(podfile).should == {
+          :changed=>["JSONKit"],
+          :removed=>[],
+          :unchanged=>["BlocksKit"],
+          :added=>[]
+        }
       end
-      @lockfile.detect_changes_with_podfile(podfile).should == {
-        :changed=>["JSONKit"],
-        :removed=>[],
-        :unchanged=>["BlocksKit"],
-        :added=>[]
-      }
-      @lockfile = Pod::Lockfile.generate(podfile, @specs)
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BlocksKit'
-        pod 'JSONKit', :git => "example2.com"
-      end
-      @lockfile.detect_changes_with_podfile(podfile).should == {
-        :changed=>["JSONKit"],
-        :removed=>[],
-        :unchanged=>["BlocksKit"],
-        :added=>[]
-      }
-    end
 
-    it "detects Pods whose head state changed" do
-      podfile = Pod::Podfile.new do
-        platform :ios
-        pod 'BlocksKit'
-        pod 'JSONKit', :head
-      end
-      @lockfile.detect_changes_with_podfile(podfile).should == {
-        :changed=>["JSONKit"],
-        :removed=>[],
-        :unchanged=>["BlocksKit"],
-        :added=>[]
-      }
-      @specs = [
-        Pod::Specification.new do |s|
-          s.name = "BlocksKit"
-          s.version = "1.0.0"
-        end,
-        Pod::Specification.new do |s|
-          s.name = "JSONKit"
-          s.version = "1.4"
-          s.version.head = true
-        end ]
+      it "detects Pods whose head state changed" do
+        podfile = Podfile.new do
+          platform :ios
+          pod 'BlocksKit'
+          pod 'JSONKit', :head
+        end
+        @lockfile.detect_changes_with_podfile(podfile).should == {
+          :changed=>["JSONKit"],
+          :removed=>[],
+          :unchanged=>["BlocksKit"],
+          :added=>[]
+        }
+        @specs = [
+          Specification.new do |s|
+            s.name = "BlocksKit"
+            s.version = "1.0.0"
+          end,
+          Specification.new do |s|
+            s.name = "JSONKit"
+            s.version = "1.4"
+            s.version.head = true
+          end ]
         @specs.each { |s| s.activate_platform(:ios) }
-        @lockfile = Pod::Lockfile.generate(podfile, @specs)
-        podfile = Pod::Podfile.new do
+        @lockfile = Lockfile.generate(podfile, @specs)
+        podfile = Podfile.new do
           platform :ios
           pod 'BlocksKit'
           pod 'JSONKit'
@@ -304,6 +266,103 @@ describe Pod::Lockfile do
           :unchanged=>["BlocksKit"],
           :added=>[]
         }
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "Serialization" do
+      before do
+        @lockfile = Lockfile.generate(Sample.podfile, Sample.specs)
+      end
+
+      it "generates a valid YAML representation" do
+        YAML.load(@lockfile.to_yaml).should == YAML.load(Sample.yaml)
+      end
+
+      it "serializes correctly `:head' dependencies" do
+        podfile = Podfile.new do
+          platform :ios
+          pod 'BananaLib', :head
+        end
+        specs = [
+          Specification.new do |s|
+            s.name = "BananaLib"
+            s.version = "1.0"
+          end,
+          Specification.new do |s|
+            s.name = "monkey"
+            s.version = "1.0.8"
+          end
+        ]
+        specs.each { |s| s.activate_platform(:ios) }
+        lockfile = Lockfile.generate(podfile, specs)
+        lockfile.internal_data["DEPENDENCIES"][0].should == "BananaLib (HEAD)"
+      end
+
+      it "serializes correctly external dependencies" do
+        podfile = Podfile.new do
+          platform :ios
+          pod 'BananaLib', { :git => "www.example.com", :tag => '1.0' }
+        end
+        specs = [
+          Specification.new do |s|
+            s.name = "BananaLib"
+            s.version = "1.0"
+          end,
+          Specification.new do |s|
+            s.name = "monkey"
+            s.version = "1.0.8"
+          end
+        ]
+        specs.each { |s| s.activate_platform(:ios) }
+        lockfile = Lockfile.generate(podfile, specs)
+        lockfile.internal_data["DEPENDENCIES"][0].should == "BananaLib (from `www.example.com', tag `1.0')"
+        lockfile.internal_data["EXTERNAL SOURCES"]["BananaLib"].should == { :git => "www.example.com", :tag => '1.0' }
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "Generation from a Podfile" do
+      before do
+        @lockfile = Lockfile.generate(Sample.podfile, Sample.specs)
+      end
+
+      it "stores the information of the installed pods and of their dependencies" do
+        @lockfile.internal_data['PODS'].should == [
+          {"BananaLib (1.0)"=>["monkey (< 1.0.9, ~> 1.0.1)"]},
+          "JSONKit (1.4)",
+          "monkey (1.0.8)"
+        ]
+      end
+
+      it "stores the information of the dependencies of the Podfile" do
+        @lockfile.internal_data['DEPENDENCIES'].should == [
+          "BananaLib (~> 1.0)", "JSONKit (from `path/JSONKit.podspec')"
+        ]
+      end
+
+      it "stores the information of the external sources" do
+        @lockfile.internal_data['EXTERNAL SOURCES'].should == {
+          "JSONKit"=>{:podspec=>"path/JSONKit.podspec"}
+        }
+      end
+
+      it "stores the checksum of the specifications" do
+        @lockfile.internal_data['SPEC CHECKSUMS'].should == {
+          "BananaLib"=>"9906b267592664126923875ce2c8d03824372c79",
+          "JSONKit"=>"92ae5f71b77c8dec0cd8d0744adab79d38560949"
+        }
+      end
+
+      it "store the version of the CocoaPods Core gem" do
+        @lockfile.internal_data['COCOAPODS CORE'].should == CORE_VERSION
+      end
+
+      it "it includes all the information that it is expected to store" do
+        @lockfile.internal_data.should == YAML.load(Sample.yaml)
+      end
     end
   end
 end
