06b6e52984ce2a20741773344c46f198350f25a4
Marin Usalj <mneorr@gmail.com>
2012-12-07 19:01:45 -0800

    override == , improved specs

--- spec/podfile/dsl_spec.rb
+++ spec/podfile/dsl_spec.rb
@@ -10,7 +10,7 @@ module Pod
         end
         podfile.dependencies.size.should == 2
         podfile.dependencies.find {|d| d.root_name == 'ASIHTTPRequest'}.should == Dependency.new('ASIHTTPRequest')
-        podfile.dependencies.find {|d| d.root_name == 'SSZipArchive'}.should   == Dependency.new('SSZipArchive', '>= 0.1')
+        podfile.dependencies.find {|d| d.root_name == 'SSZipArchive'}.should   == Dependency.new('SSZipArchive', '>= 0.1',  inhibit_warnings: true)
       end
 
       it "adds a dependency on a Pod repo outside of a spec repo (the repo is expected to contain a podspec)" do
