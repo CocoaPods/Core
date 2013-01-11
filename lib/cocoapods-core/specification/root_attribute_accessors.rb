fa9e7669441cf3ecf76603f314a5182e2ce9be9f
Fabio Pelosin <fabio@Fabios-iMac-2.local>
2013-01-11 17:50:06 +0100

    [RootAttributesAccessors] Add module to handle root attributes.
    With this module there is no need to use meta programming for root attributes.

---
+++ lib/cocoapods-core/specification/root_attribute_accessors.rb
@@ -0,0 +1,124 @@
+module Pod
+  class Specification
+    module DSL
+
+      # Provides the accessors methods for the root attributes. Root attributes
+      # do not support multiplatform values and inheritance.
+      #
+      module RootAttributesAccessors
+
+      # @return [String] The name of the specification *not* including the
+      #         names of the parents, in case of ‘sub-specifications’.
+      #
+      def base_name
+        attributes_hash[:name]
+      end
+
+      # @return [String] The name of the specification including the names of
+      #         the parents, in case of ‘sub-specifications’.
+      #
+      def name
+        parent ? "#{parent.name}/#{base_name}" : base_name
+      end
+
+      # @return [Version] The version of the Pod.
+      #
+      def version
+        Version.new(attributes_hash[:version])
+      end
+
+      # @return [Hash] a hash containing the authors as the keys and their
+      #         email address as the values.
+      #
+      # @note   The value is coherced to a hash with a nil email if needed.
+      #
+      # @example Possible values
+      #
+      #   { 'Author' => 'email@host.com' }
+      #   [ 'Author', { 'Author_2' => 'email@host.com' } ]
+      #   [ 'Author', 'Author_2' ]
+      #   'Author'
+      #
+      def authors
+        authors = attributes_hash[:authors]
+        if authors.is_a?(Hash)
+          authors
+        elsif authors.is_a?(Array)
+          result = {}
+          authors.each do |name_or_hash|
+             if name_or_hash.is_a?(String)
+               result[name_or_hash] = nil
+             else
+               result.merge!(name_or_hash)
+             end
+           end
+          result
+        elsif authors.is_a?(String)
+          { authors => nil }
+        end
+      end
+
+      # @return [Hash] A hash containing the license information of the Pod.
+      #
+      # @note   The indentation is stripped from the license text.
+      #
+      def license
+        license = attributes_hash[:license]
+        if license.is_a?(String)
+          { :type => license }
+        else
+          license[:text] = license[:text].strip_heredoc if license[:text]
+          license
+        end
+      end
+
+      # @return [String] The URL of the homepage of the Pod.
+      #
+      def homepage
+        attributes_hash[:homepage]
+      end
+
+      # @return [Hash{Symbol=>String}] The location from where the library
+      #         should be retrieved.
+      #
+      def source
+        attributes_hash[:source]
+      end
+
+      # @return [String] A short description of the Pod.
+      #
+      def summary
+        attributes_hash[:summary]
+      end
+
+      # @return [String] A longer description of the Pod.
+      #
+      # @note   The indentation is stripped from the description.
+      #
+      def description
+        attributes_hash[:description].strip_heredoc
+      end
+
+      # @return [Array<String>] The list of the URL for the screenshots of the
+      #         Pod.
+      #
+      # @note   The value is coherced to an array.
+      #
+      def screenshots
+        value = attributes_hash[:screenshots]
+        [*value]
+      end
+
+      # @return [Hash{Symbol=>Array<String>}] The options to pass to the
+      #         appledoc tool.
+      #
+      def documentation
+        attributes_hash[:documentation]
+      end
+
+      #-----------------------------------------------------------------------#
+
+      end
+    end
+  end
+end
---
+++ spec/specification/root_attribute_accessors_spec.rb
@@ -0,0 +1,114 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+module Pod
+  describe Specification::RootAttributesAccessors do
+
+    before do
+      @spec = Spec.new do |s|
+        s.name = "Pod"
+        s.version = '1.0'
+        s.subspec 'Subspec' do
+        end
+      end
+    end
+
+    it "returns the name of the specification" do
+      @spec.name.should == 'Pod'
+      @spec.subspecs.first.name.should == 'Pod/Subspec'
+    end
+
+    it "returns the base name of the specification" do
+      @spec.base_name.should == 'Pod'
+      @spec.subspecs.first.base_name.should == 'Subspec'
+    end
+
+    it "returns the version of the specification" do
+      @spec.version.should == Version.new('1.0')
+    end
+
+    it "returns the authors" do
+      hash = { 'Darth Vader' => 'darthvader@darkside.com',
+               'Wookiee' => 'wookiee@aggrrttaaggrrt.com' }
+      @spec.authors = hash
+      @spec.authors.should == hash
+    end
+
+    it "supports the author attribute specified as an array" do
+      @spec.authors = 'Darth Vader', 'Wookiee'
+      @spec.authors.should == { 'Darth Vader' => nil, 'Wookiee' => nil }
+    end
+
+    it "supports the author attribute specified as a string" do
+      @spec.authors = 'Darth Vader'
+      @spec.authors.should == { 'Darth Vader' => nil }
+    end
+
+    it "supports the author attribute specified as an array of strings and hashes" do
+      @spec.authors = [ 'Darth Vader',
+                        { 'Wookiee' => 'wookiee@aggrrttaaggrrt.com' } ]
+      @spec.authors.should == {
+        'Darth Vader' => nil,
+        'Wookiee' => 'wookiee@aggrrttaaggrrt.com'
+      }
+    end
+
+    it "supports the license attribute specified as a string" do
+      @spec.license = 'MIT'
+      @spec.license.should == { :type => 'MIT' }
+    end
+
+    it "supports the license attribute specified as a hash" do
+      @spec.license = { :type => 'MIT', :file => 'MIT-LICENSE' }
+      @spec.license.should == { :type => 'MIT', :file => 'MIT-LICENSE' }
+    end
+
+    it "strips indenetation from the license text" do
+      text = <<-DOC
+        Line1
+        Line2
+      DOC
+      @spec.license = { :type => 'MIT', :text => text }
+      @spec.license[:text].should == "Line1\nLine2\n"
+    end
+
+    it "returns the homepage" do
+      @spec.homepage = 'www.example.com'
+      @spec.homepage.should == 'www.example.com'
+    end
+
+    it "returns the source" do
+      @spec.source = { :git => 'www.example.com/repo.git' }
+      @spec.source.should == { :git => 'www.example.com/repo.git' }
+    end
+
+    it "returns the summary" do
+      @spec.summary = 'A library that describes the meaning of life.'
+      @spec.summary.should == 'A library that describes the meaning of life.'
+    end
+
+    it "returns the descriptions stripping indentation" do
+      desc = <<-DESC
+        Line1
+        Line2
+      DESC
+      @spec.description = desc
+      @spec.description.should == "Line1\nLine2\n"
+    end
+
+    it "returns the screenshots" do
+      @spec.screenshots = ['www.example.com/img1.png', 'www.example.com/img2.png']
+      @spec.screenshots.should == ['www.example.com/img1.png', 'www.example.com/img2.png']
+    end
+
+    it "support the specification of the attribute as a string" do
+      @spec.screenshot = 'www.example.com/img1.png'
+      @spec.screenshots.should == ['www.example.com/img1.png']
+    end
+
+    it "returns any setting to pass to the appledoc tool" do
+      settings =  { :appledoc => ['--no-repeat-first-par', '--no-warn-invalid-crossref'] }
+      @spec.documentation = settings
+      @spec.documentation.should == settings
+    end
+  end
+end
