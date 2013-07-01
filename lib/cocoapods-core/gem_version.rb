a0c0e21dc3e4895c9408e0ec4e2a4e233a17acd4
Fabio Pelosin <fabiopelosin@gmail.com>
2013-07-01 13:40:38 +0200

    Release 0.21.0

--- lib/cocoapods-core/gem_version.rb
+++ lib/cocoapods-core/gem_version.rb
@@ -2,5 +2,5 @@ module Pod
 
   # The version of the cocoapods-core.
   #
-  CORE_VERSION = '0.21.0.rc1' unless defined? Pod::CORE_VERSION
+  CORE_VERSION = '0.21.0' unless defined? Pod::CORE_VERSION
 end
