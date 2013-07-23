b56cbd3145fbf97ff9589d70f7bc6f8b88c9a626
Eloy Durán <eloy.de.enige@gmail.com>
2013-07-23 22:38:45 +0200

    Release 0.22.3

--- lib/cocoapods-core/gem_version.rb
+++ lib/cocoapods-core/gem_version.rb
@@ -2,5 +2,5 @@ module Pod
 
   # The version of the cocoapods-core.
   #
-  CORE_VERSION = '0.22.2' unless defined? Pod::CORE_VERSION
+  CORE_VERSION = '0.22.3' unless defined? Pod::CORE_VERSION
 end
