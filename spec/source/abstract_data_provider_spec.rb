bcac693f2d4a91753a155509c1cac251c3d998f1
Fabio Pelosin <fabiopelosin@gmail.com>
2013-12-02 04:45:11 +0100

    [Source] Add AbstractDataProvider

---
+++ spec/source/abstract_data_provider_spec.rb
@@ -0,0 +1,54 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+module Pod
+  describe Source::AbstractDataProvider do
+
+    before do
+      @sut = Source::AbstractDataProvider.new
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "Optional methods" do
+      it "raises for the #name method" do
+        should.raise StandardError do
+          @sut.name
+        end.message.should.match /Abstract method/
+      end
+
+      it "raises for the #type method" do
+        should.raise StandardError do
+          @sut.type
+        end.message.should.match /Abstract method/
+      end
+
+      it "raises for the #pods method" do
+        should.raise StandardError do
+          @sut.pods
+        end.message.should.match /Abstract method/
+      end
+
+      it "raises for the #versions method" do
+        should.raise StandardError do
+          @sut.versions('Pod')
+        end.message.should.match /Abstract method/
+      end
+
+      it "raises for the #specification method" do
+        should.raise StandardError do
+          @sut.specification('Pod', '0.1.0')
+        end.message.should.match /Abstract method/
+      end
+
+      it "raises for the #specification_contents method" do
+        should.raise StandardError do
+          @sut.specification_contents('Pod', '0.1.0')
+        end.message.should.match /Abstract method/
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+  end
+end
+
