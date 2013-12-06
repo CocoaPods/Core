6b3de0020207a2c73c7549969813dcff53ecca19
Joshua Kalpin <joshua.kalpin@gmail.com>
2013-12-06 10:50:20 -0800

    Move the Linter::Result class into its own file.
    Clears up a bit of the clutter inside of linter.rb and makes it clearly
    it's own entity within the linter.

--- lib/cocoapods-core/specification/linter.rb
+++ lib/cocoapods-core/specification/linter.rb
@@ -1,3 +1,5 @@
+require 'cocoapods-core/specification/linter/result'
+
 module Pod
   class Specification
 
@@ -500,49 +502,6 @@ module Pod
         end
         result.platforms << consumer.platform_name if consumer
       end
-
-      #-----------------------------------------------------------------------#
-
-      class Result
-
-        # @return [Symbol] the type of result.
-        #
-        attr_reader :type
-
-        # @return [String] the message associated with result.
-        #
-        attr_reader :message
-
-        # @param [Symbol] type    @see type
-        # @param [String] message @see message
-        #
-        def initialize(type, message)
-          @type    = type
-          @message = message
-          @platforms = []
-        end
-
-        # @return [Array<Platform>] the platforms where this result was
-        #         generated.
-        #
-        attr_reader :platforms
-
-        # @return [String] a string representation suitable for UI output.
-        #
-        def to_s
-          r = "[#{type.to_s.upcase}] #{message}"
-          if platforms != Specification::PLATFORMS
-            platforms_names = platforms.uniq.map do |p|
-              Platform.string_name(p)
-            end
-            r << " [#{platforms_names * ' - '}]" unless platforms.empty?
-          end
-          r
-        end
-      end
-
-      #-----------------------------------------------------------------------#
-
     end
   end
 end
