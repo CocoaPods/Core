87ea446c9b28659e8315988f29803497fc28fce9
Boris Bügling <boris@icculus.org>
2014-04-08 16:25:27 +0200

    Support repos with dots in the name.

--- lib/cocoapods-core/github.rb
+++ lib/cocoapods-core/github.rb
@@ -103,7 +103,7 @@ module Pod
     # @return [Nil] if the given url is not a valid github repo url.
     #
     def self.repo_id_from_url(url)
-      url[%r{github.com/([^/\.]*/[^/\.]*)\.*}, 1]
+      url[%r{github.com/([^/]*/[^/]*)\.*}, 1]
     end
 
     # Performs a get request with the given URL.
