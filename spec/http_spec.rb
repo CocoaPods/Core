5d0fce6bbe2a35d8f46fa2680d648c4e2f77fde9
Boris Bügling <boris@icculus.org>
2014-04-20 21:08:59 +0200

    Fixed code style offenses.

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -7,7 +7,7 @@ module Pod
       it 'can resolve redirects' do
         WebMock::API.stub_request(:head, /redirect/).to_return(
           :status => 301, :headers => { 'Location' => 'http://NEW-URL/' })
-        WebMock::API.stub_request(:head, /new-url/).to_return( :status => 200 )
+        WebMock::API.stub_request(:head, /new-url/).to_return(:status => 200)
 
         resolved_url = HTTP.get_actual_url('http://SOME-URL/redirect/')
         resolved_url.should == 'http://NEW-URL/'
@@ -16,30 +16,30 @@ module Pod
       it 'can resolve relative redirects' do
         WebMock::API.stub_request(:head, /redirect/).to_return(
           :status => 301, :headers => { 'Location' => '/foo' })
-        WebMock::API.stub_request(:head, /foo/).to_return( :status => 200 )
+        WebMock::API.stub_request(:head, /foo/).to_return(:status => 200)
 
         resolved_url = HTTP.get_actual_url('http://SOME-URL/redirect')
         resolved_url.should == 'http://SOME-URL/redirect/foo'
       end
 
       it 'can successfully validate URLs' do
-        WebMock::API.stub_request(:head, /foo/).to_return( :status => 200 )
+        WebMock::API.stub_request(:head, /foo/).to_return(:status => 200)
 
         response = HTTP.validate_url('http://SOME-URL/foo')
         response.success?.should.be.true
       end
 
       it 'is resilient to HEAD errros' do
-        WebMock::API.stub_request(:head, /foo/).to_return( :status => 404 )
-        WebMock::API.stub_request(:get, /foo/).to_return( :status => 200 )
+        WebMock::API.stub_request(:head, /foo/).to_return(:status => 404)
+        WebMock::API.stub_request(:get, /foo/).to_return(:status => 200)
 
         response = HTTP.validate_url('http://SOME-URL/foo')
         response.success?.should.be.true
       end
 
       it 'reports failures when validating URLs' do
-        WebMock::API.stub_request(:head, /foo/).to_return( :status => 404 )
-        WebMock::API.stub_request(:get, /foo/).to_return( :status => 404 )
+        WebMock::API.stub_request(:head, /foo/).to_return(:status => 404)
+        WebMock::API.stub_request(:get, /foo/).to_return(:status => 404)
 
         response = HTTP.validate_url('http://SOME-URL/foo')
         response.success?.should.be.false
