15089f022e4a09befb002f059d6c3a283724b275
Olivier Halligon <olivier@halligon.net>
2014-05-28 23:33:49 +0200

    Better handling of invalid URL validation (fix CocoaPods/CocoaPods#2197)

--- lib/cocoapods-core/http.rb
+++ lib/cocoapods-core/http.rb
@@ -42,7 +42,7 @@ module Pod
       begin
         url = get_actual_url(url)
         resp = perform_head_request(url)
-      rescue SocketError
+      rescue SocketError, URI::InvalidURIError
         resp = nil
       end
 
