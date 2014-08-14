ca27f82ae6599151c9d8d95e682a9e43abcee6e7
Fabio Pelosin <fabiopelosin@gmail.com>
2014-08-14 15:10:37 +0200

    [Spec] Configure VRC to ignore codeclimate.com

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = ROOT + 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
+  c.ignore_hosts 'codeclimate.com'
 end
 
 # Silence the output
