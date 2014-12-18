24d5b7eba5ded5d2d41df22a112b36952d35eec9
ratazzi <ratazzi.potts@gmail.com>
2014-12-18 10:19:39 +0800

    Revert "Fix Unsupported class for YAML conversion Fixnum when `write_lockfiles`"
    This reverts commit d12fa28324424270e9f29ff4a21e73caec5314a5.

--- lib/cocoapods-core/lockfile.rb
+++ lib/cocoapods-core/lockfile.rb
@@ -294,19 +294,6 @@ module Pod
       self.defined_in_file = path
     end
 
-    # @return [Hash{String=>Array,Hash,String}] Convert Fixnum to String
-    #
-    def clean_node(node)
-      case node
-
-      when Fixnum then node = node.to_s
-      when Array then node.map! { |obj| clean_node(obj) }
-      when Hash then node.each_pair { |key, value| node[key] = clean_node(value) }
-      end
-
-      node
-    end
-
     # @return [Hash{String=>Array,Hash,String}] a hash representation of the
     #         Lockfile.
     #
@@ -328,7 +315,7 @@ module Pod
     def to_hash
       hash = {}
       internal_data.each do |key, value|
-        hash[key] = clean_node(value) unless value.empty?
+        hash[key] = value unless value.empty?
       end
       hash
     end
