24356131dcf4dcbc9e507299eadd0a7886f1a846
Kyle Fuller <kyle@fuller.li>
2015-03-11 01:02:56 +0000

    Release 0.36.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # CocoaPods Core Changelog
 
-## Master
+## 0.36.0
 
 ##### Bug Fixes
 
--- lib/cocoapods-core/gem_version.rb
+++ lib/cocoapods-core/gem_version.rb
@@ -1,5 +1,5 @@
 module Pod
   # The version of the cocoapods-core.
   #
-  CORE_VERSION = '0.36.0.rc.1' unless defined? Pod::CORE_VERSION
+  CORE_VERSION = '0.36.0' unless defined? Pod::CORE_VERSION
 end
