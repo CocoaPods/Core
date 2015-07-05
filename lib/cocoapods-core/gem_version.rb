c7d7b5c71dfeeb24b92a22835569f6624c81d43b
Samuel E. Giddins <segiddins@segiddins.me>
2015-07-05 11:42:22 -0700

    Release 0.38.0.beta.2

--- lib/cocoapods-core/gem_version.rb
+++ lib/cocoapods-core/gem_version.rb
@@ -1,5 +1,5 @@
 module Pod
   # The version of the cocoapods-core.
   #
-  CORE_VERSION = '0.38.0.beta.1' unless defined? Pod::CORE_VERSION
+  CORE_VERSION = '0.38.0.beta.2' unless defined? Pod::CORE_VERSION
 end
