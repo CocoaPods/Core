4314855f70c33235b1572c16a3e673ebbdf45e80
Zoltán Matók <zolomatok@users.noreply.github.com>
2016-07-27 17:08:50 +0200

    Removed trailing whitespace (hopefully)

--- lib/cocoapods-core/specification/dsl.rb
+++ lib/cocoapods-core/specification/dsl.rb
@@ -50,14 +50,14 @@ module Pod
     #       spec.summary      = 'ARC and GCD Compatible Reachability Class for iOS and OS X.'
     #       spec.source       = { :git => 'https://github.com/tonymillion/Reachability.git', :tag => 'v3.1.0' }
     #       spec.module_name  = 'Rich'
-    #       
+    #
     #       spec.ios.deployment_target  = '9.0'
     #       spec.osx.deployment_target  = '10.10'
     #
     #       spec.source_files       = 'Reachability/common/*.swift'
     #       spec.ios.source_files   = 'Reachability/ios/*.swift', 'Reachability/extensions/*.swift'
     #       spec.osx.source_files   = 'Reachability/osx/*.swift'
-    #       
+    #
     #       spec.framework      = 'SystemConfiguration'
     #       spec.ios.framework  = 'UIKit'
     #       spec.osx.framework  = 'AppKit'
