CocoaPods/Core

commit 559584456461e929871e16596971d1b384c86e7f

Cache specification hash value

lib/cocoapods-core/specification.rb
@@ -25,6 +25,10 @@ module Pod
     #
     attr_reader :parent
 
+    # @return [Integer] the cached hash value for this spec.
+    #
+    attr_reader :hash_value
+
     # @param  [Specification] parent @see parent
     #
     # @param  [String] name
@@ -35,6 +39,7 @@ module Pod
       @subspecs = []
       @consumers = {}
       @parent = parent
+      @hash_value = nil
       attributes_hash['name'] = name
 
       yield self if block_given?
@@ -80,7 +85,10 @@ module Pod
     # @return [Fixnum] The hash value.
     #
     def hash
-      (name.hash * 53) ^ version.hash
+      if @hash_value.nil?
+        @hash_value = (name.hash * 53) ^ version.hash
+      end
+      @hash_value
     end
 
     # @return [String] A string suitable for representing the specification in
@@ -580,6 +588,35 @@ module Pod
         raise StandardError, 'Defined in file can be set only for root specs.'
       end
       @defined_in_file = file
+    end
+
+    # Sets the name of the `podspec`.
+    #
+    # @param  [String] name
+    #         the `podspec` name.
+    #
+    # @return [void]
+    #
+    # @visibility private
+    #
+    def name=(name)
+      @hash_value = nil
+      attributes_hash['name'] = name
+    end
+
+    # Sets the version of the `podspec`.
+    #
+    # @param  [String] version
+    #         the `podspec` version.
+    #
+    # @return [void]
+    #
+    # @visibility private
+    #
+    def version=(version)
+      @hash_value = nil
+      attributes_hash['version'] = version
+      @version = nil
     end
 
     # @!group Validation

spec/specification_spec.rb
@@ -79,6 +79,26 @@ module Pod
         hash[spec_1].should == 'VALUE_2'
       end
 
+      it 'resets hash value when name changes' do
+        @spec.hash_value.should.be.nil?
+        original_hash = @spec.hash
+        @spec.hash_value.should.not.be.nil?
+        @spec.name = 'NewPodName'
+        @spec.hash_value.should.be.nil?
+        new_hash = @spec.hash
+        original_hash.should != new_hash
+      end
+
+      it 'resets hash value when version changes' do
+        @spec.hash_value.should.be.nil?
+        original_hash = @spec.hash
+        @spec.hash_value.should.not.be.nil?
+        @spec.version = '1.1'
+        @spec.hash_value.should.be.nil?
+        new_hash = @spec.hash
+        original_hash.should != new_hash
+      end
+
       it 'produces a string representation suitable for UI output.' do
         @spec.to_s.should == 'Pod (1.0)'
       end

commit 83a447531f19c6de84acc47bdb910b6d4e4337fb

[Specification] Strip newlines for #version
Fixes regression caused in 559584456461e929871e16596971d1b384c86e7f

lib/cocoapods-core/specification.rb
@@ -615,7 +615,7 @@ module Pod
     #
     def version=(version)
       @hash_value = nil
-      attributes_hash['version'] = version
+      store_attribute(:version, version)
       @version = nil
     end
 

spec/specification_spec.rb
@@ -79,6 +79,15 @@ module Pod
         hash[spec_1].should == 'VALUE_2'
       end
 
+      it 'strips newlines from versions' do
+        spec = Spec.new do |s|
+          s.name = 'Pod'
+          s.version = "1.2.0\n"
+        end
+
+        spec.to_hash['version'].should == '1.2.0'
+      end
+
       it 'resets hash value when name changes' do
         @spec.hash_value.should.be.nil?
         original_hash = @spec.hash