5c700e2213e89de1c1ef32e35f2d2c7dbbf88479
Kyle Fleming <kyle@kylefleming.net>
2018-01-24 16:06:54 -0800

    Add Range header to HTTP::perform_head_request
    Uses a GET request with a 1 byte Range header if the initial HEAD request fails. Falls back to a GET request without a Range header if that also fails.

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -2,6 +2,10 @@ require File.expand_path('../spec_helper', __FILE__)
 
 module Pod
   describe HTTP do
+    after do
+      WebMock.reset!
+    end
+
     describe 'In general' do
       it 'can resolve redirects' do
         WebMock::API.stub_request(:head, /redirect/).to_return(
@@ -28,7 +32,7 @@ module Pod
         response.success?.should.be.true
       end
 
-      it 'is resilient to HEAD errros' do
+      it 'is resilient to HEAD errors' do
         WebMock::API.stub_request(:head, /foo/).to_return(:status => 404)
         WebMock::API.stub_request(:get, /foo/).to_return(:status => 200)
 
@@ -50,9 +54,33 @@ module Pod
       end
 
       it 'uses a browser user-agent to validate URLs' do
-        WebMock::API.stub_request(:get, 'http://SOME-URL/foo').
+        WebMock::API.stub_request(:head, /foo/).to_return(:status => 404)
+        WebMock::API.stub_request(:get, /foo/).
           with(:headers => { :user_agent => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/538.43.40 (KHTML, like Gecko) Version/8.0 Safari/538.43.40' }).
           to_return(:status => 200, :body => '')
+
+        response = HTTP.validate_url('http://SOME-URL/foo')
+        response.success?.should.be.true
+      end
+
+      it 'uses GET with Range of 1 byte' do
+        WebMock::API.stub_request(:head, /foo/).to_return(:status => 404)
+        WebMock::API.stub_request(:get, /foo/).
+            with(:headers => { :range => 'bytes=0-0' }).
+            to_return(:status => 200, :body => ' ')
+
+        response = HTTP.validate_url('http://SOME-URL/foo')
+        response.success?.should.be.true
+      end
+
+      it 'uses GET without Range if server rejects Range' do
+        WebMock::API.stub_request(:head, /foo/).to_return(:status => 404)
+        WebMock::API.stub_request(:get, /foo/).
+            with(:headers => { :range => 'bytes=0-0' }).
+            to_return(:status => 500)
+        WebMock::API.stub_request(:get, /foo/).
+            with { |request| request.headers.nil? || !request.headers.key?('Range') }.
+            to_return(:status => 200, :body => '')
 
         response = HTTP.validate_url('http://SOME-URL/foo')
         response.success?.should.be.true
