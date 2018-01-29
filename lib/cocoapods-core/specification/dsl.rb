d0424c9046ecfce3c5c17ab1f0a641eba4a6de54
William Entriken <github.com@phor.net>
2018-01-29 16:28:05 -0500

    Show swift_version example in the documentation

--- lib/cocoapods-core/specification/dsl.rb
+++ lib/cocoapods-core/specification/dsl.rb
@@ -42,14 +42,15 @@ module Pod
     # Or it can be quite detailed:
     #
     #     Pod::Spec.new do |spec|
-    #       spec.name         = 'Reachability'
-    #       spec.version      = '3.1.0'
-    #       spec.license      = { :type => 'BSD' }
-    #       spec.homepage     = 'https://github.com/tonymillion/Reachability'
-    #       spec.authors      = { 'Tony Million' => 'tonymillion@gmail.com' }
-    #       spec.summary      = 'ARC and GCD Compatible Reachability Class for iOS and OS X.'
-    #       spec.source       = { :git => 'https://github.com/tonymillion/Reachability.git', :tag => 'v3.1.0' }
-    #       spec.module_name  = 'Rich'
+    #       spec.name          = 'Reachability'
+    #       spec.version       = '3.1.0'
+    #       spec.license       = { :type => 'BSD' }
+    #       spec.homepage      = 'https://github.com/tonymillion/Reachability'
+    #       spec.authors       = { 'Tony Million' => 'tonymillion@gmail.com' }
+    #       spec.summary       = 'ARC and GCD Compatible Reachability Class for iOS and OS X.'
+    #       spec.source        = { :git => 'https://github.com/tonymillion/Reachability.git', :tag => 'v3.1.0' }
+    #       spec.module_name   = 'Rich'
+    #       spec.swift_version = '4.0'
     #
     #       spec.ios.deployment_target  = '9.0'
     #       spec.osx.deployment_target  = '10.10'
