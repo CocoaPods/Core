9cc8f91cda33be8473f7517df9509f8764088e85
Eric Amorde <eric.amorde@gmail.com>
2020-06-07 11:27:19 -0700

    Remove explicit activesupport from Gemfile, update all gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,4 @@
 
-# Set up coverage analysis
-#-----------------------------------------------------------------------------#
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.configure do |config|
-  config.logger.level = Logger::WARN
-end
-CodeClimate::TestReporter.start
-
 # Set up
 #-----------------------------------------------------------------------------#
 
