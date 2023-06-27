478257924ecd346efe957c8f0d8515a0e6f00c59
Srini <m.srinivas1729@gmail.com>
2023-06-27 08:47:56 -0700

    Keeping the regex /github.com/ for compatibility reasons

--- lib/cocoapods-core/source.rb
+++ lib/cocoapods-core/source.rb
@@ -467,7 +467,7 @@ module Pod
     end
 
     def unchanged_github_repo?
-      return unless url =~ /github\.com[:\/]CocoaPods\/CocoaPods\.git/
+      return unless url =~ /github.com/
       !GitHub.modified_since_commit(url, git_commit_hash)
     end
 
